db491a61a73d35da708d365b7ab90ff11af98f0b
darvishiyan <darvishiyan@gmail.com>
2023-09-05 12:49:25 -0700

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,17 @@ plugins {
     id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("org.jetbrains.kotlin.jvm") version "1.8.0" apply false
-    id("com.google.dagger.hilt.android") version "2.45" apply false
+    id("com.google.dagger.hilt.android") version "2.47" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.1" apply false
     kotlin("kapt") version "1.9.10"
 }
 allprojects {
     ext {
         set("core_ktx_version", "1.10.1")
         set("junit_version", "4.13.2")
-        set("hilt_version", "2.45")
+        set("hilt_version", "2.47")
         set("retrofit_version", "2.9.0")
         set("okhttp_version", "4.10.0")
+        set("navigation_version", "2.7.1")
     }
 }
